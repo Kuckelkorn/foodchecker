f691604de9055bbbe233011419a4bc21e9ed4f49
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-02-14 13:16:09 +0100

    rendering states

--- helpers/render.js
+++ helpers/render.js
@@ -1,4 +1,7 @@
-const render = (product) => {
+import getData from "./getData.js"
+
+const renderProduct = (product) => {
+  const camera = document.querySelector('#camera')
   const title = product.product_name
   const nutrition = product.nutriscore_data // OBJECT
   const allergies = product.allergens_hierarchy // ARRAY
@@ -6,6 +9,7 @@ const render = (product) => {
   console.log(allergies)
   console.log(nutrition.score)
 
+  camera.classList.toggle('hidden')
   const header = document.querySelector('header')
   const main = document.querySelector('main')
   header.innerHTML = `<h1>${title}</h1>`
@@ -19,4 +23,18 @@ const render = (product) => {
     </section>`
 }
 
-export default render
+export const renderCamera = async () => {
+  const product = await getData(737628064502);
+
+  const main = document.querySelector('main')
+  const title = document.querySelector('header')
+  const camera = document.querySelector('#camera')
+
+  camera.classList.toggle('hidden')
+
+  title.innerHTML = "<h1>Foodchecker</h1>"
+  main.innerHTML = `<form><input type="text"><button id="search">Zoek</button></form>`
+
+  const search = document.querySelector('#search')
+  search.addEventListener('click', async () => renderProduct( await product))
+}
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import getData from './helpers/getData.js'
-import render from './helpers/render.js';
+import {renderCamera} from './helpers/render.js';
 
-const product = await getData(737628064502);
 
-render(await product)
+const camera = document.querySelector('#camera')
+
+camera.addEventListener('click', () => renderCamera())
