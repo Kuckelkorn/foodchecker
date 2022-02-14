9fed5f4420a2a705c693e76a3903d282d6ee9592
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-02-14 14:27:11 +0100

    trying barcode scan

--- helpers/render.js
+++ helpers/render.js
@@ -1,40 +1,46 @@
 import getData from "./getData.js"
 
 const renderProduct = (product) => {
-  const camera = document.querySelector('#camera')
-  const title = product.product_name
-  const nutrition = product.nutriscore_data // OBJECT
-  const allergies = product.allergens_hierarchy // ARRAY
-
-  console.log(allergies)
-  console.log(nutrition.score)
-
-  camera.classList.toggle('hidden')
+  const footer = document.querySelector('footer')
   const header = document.querySelector('header')
   const main = document.querySelector('main')
-  header.innerHTML = `<h1>${title}</h1>`
+
+  // Filter necessary items
+  const title =  product.generic_name
+  const productName = product.product_name
+  const nutrition =  product.nutriscore_data // OBJECT
+  const allergies =  product.allergens_hierarchy // ARRAY
+
+  footer.classList.toggle('hidden')
+
+  // Changing the HTML with the API data
+  header.innerHTML = `<h1>${ title}</h1><p>${productName}</p>`
   main.innerHTML = `<section class="voedingswaarde">
     <h2>Voedingswaarde</h2>
-    <h3>Score: ${nutrition.grade}</h3>
+    <h3>Score: ${ nutrition.grade}</h3>
     </section>
     <section class="allergies">
     <h2> Allergieen</h2>
-    <p>${allergies}</p>
+    <p>${ allergies}</p>
     </section>`
 }
 
 export const renderCamera = async () => {
-  const product = await getData(737628064502);
-
   const main = document.querySelector('main')
   const title = document.querySelector('header')
-  const camera = document.querySelector('#camera')
+  const footer = document.querySelector('footer')
 
-  camera.classList.toggle('hidden')
+  footer.classList.toggle('hidden')
 
+  // Changing the HTML 
   title.innerHTML = "<h1>Foodchecker</h1>"
-  main.innerHTML = `<form><input type="text"><button id="search">Zoek</button></form>`
+  main.innerHTML = `<form><label>737628064502</label><input type="text"><button id="search">Zoek</button></form>`
 
   const search = document.querySelector('#search')
-  search.addEventListener('click', async () => renderProduct( await product))
+  search.addEventListener('click', async (e) => {
+    e.preventDefault()
+    const barcode = document.querySelector('input').value
+    const product = await getData(barcode)
+    renderProduct(await product)
+  })
 }
--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 import {renderCamera} from './helpers/render.js';
-
-
 const camera = document.querySelector('#camera')
+
+
+if (!('BarcodeDetector' in window)) {
+  console.log('Barcode Detector is not supported by this browser.');
+} else {
+  console.log('Barcode Detector supported!');
+  // create new detector
+  const barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13']});
+  barcodeDetector.detect(imageEl)
+    .then(barcodes => {
+      barcodes.forEach(barcode => console.log(barcode.rawData));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
 
 camera.addEventListener('click', () => renderCamera())
